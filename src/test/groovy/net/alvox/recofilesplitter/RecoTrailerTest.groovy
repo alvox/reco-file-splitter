796adff7ab7719c763fdf37fa1940aa8b0ac3f6a
alvox <al@alvox.net>
2016-05-11 23:57:00 +0300

    RecoTrailerTest: amounts check

--- src/test/groovy/net/alvox/recofilesplitter/RecoTrailerTest.groovy
+++ src/test/groovy/net/alvox/recofilesplitter/RecoTrailerTest.groovy
@@ -14,6 +14,10 @@ class RecoTrailerTest extends Specification {
         trailer.updateAmounts("****************************************************************10000001486")
 
         then:
+        trailer.debitCount == 3
+        trailer.debitAmount == 2615
+        trailer.creditCount == 0
+        trailer.creditAmount == 0
         trailer.compose() == "900000000300000000000261E00000000000000000000000{                                                                                                                    "
     }
 
@@ -27,6 +31,10 @@ class RecoTrailerTest extends Specification {
         trailer.updateAmounts("****************************************************************14000000705")
 
         then:
+        trailer.debitCount == 0
+        trailer.debitAmount == 0
+        trailer.creditCount == 3
+        trailer.creditAmount == -10898
         trailer.compose() == "900000000000000000000000{00000000300000000001089Q                                                                                                                    "
     }
 
@@ -41,7 +49,29 @@ class RecoTrailerTest extends Specification {
         trailer.updateAmounts("****************************************************************10000000555")
 
         then:
+        trailer.debitCount == 2
+        trailer.debitAmount == 1129
+        trailer.creditCount == 2
+        trailer.creditAmount == -7249
         trailer.compose() == "900000000200000000000112I00000000200000000000724R                                                                                                                    "
+    }
+
+    def "test that amounts are empty after reset"() {
+        setup:
+        def trailer = new RecoTrailer()
+
+        when:
+        trailer.updateAmounts("****************************************************************10000000574")
+        trailer.updateAmounts("****************************************************************14000000705")
+        trailer.updateAmounts("****************************************************************14000006544")
+        trailer.updateAmounts("****************************************************************10000000555")
+        trailer.reset()
+
+        then:
+        trailer.debitCount == 0
+        trailer.debitAmount == 0
+        trailer.creditCount == 0
+        trailer.creditAmount == 0
     }
 
 }
